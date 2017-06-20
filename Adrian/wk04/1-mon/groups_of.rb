c0769f2a499f26e365b06c8030d7240fec155f7e
Adrian <adrian.mancuso@gmail.com>
2017-06-20 15:18:42 +1000

    fixed optional problem

--- Adrian/wk04/1-mon/groups_of.rb
+++ Adrian/wk04/1-mon/groups_of.rb
@@ -6,12 +6,18 @@ puts "enter a group size:"
 size = gets.chomp.to_i
 result = []
 
-random_items.each_slice(size) {|arr| result.push(arr)}
-puts result
+random_items.shuffle!.each_slice(size) {|arr| result.push(arr)}
+binding.pry
+if result.last.length < result.first.length do
+	x = result.pop
+	result.last.concat(x)
+end
+end
+puts "hello this is the #{result}"
 
 # method one
 # while random_items.length >= size do
-# 		new_arr = [].push(random_items.slice!(0, 2))
+# 		new_arr = [].push(random_items.shuffle!.slice!(0, 2))
 # 		# binding.pry	
 # end
 # puts new_arr
