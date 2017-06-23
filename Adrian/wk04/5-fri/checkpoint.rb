33275075b3de2db0fdf099ee46582e70fa187da0
Adrian <adrian.mancuso@gmail.com>
2017-06-23 11:21:25 +1000

    completed checkpoint

---
+++ Adrian/wk04/5-fri/checkpoint.rb
@@ -0,0 +1,43 @@
+require "pry"
+
+card_pack = [
+	{:name => :Pikachu, :attack => 40},
+	{:name => :Rattata, :attack => 20}, 
+	{:name => :Pidgeot, :attack => 60}, 
+	{:name => :Alakazam, :attack => 80}, 
+	{:name => :Butterfree, :attack => 30}, 
+	{:name => :Gengar, :attack => 70}, 
+	{:name => :Moltres, :attack => 100}, 
+	{:name => :Vulpix, :attack => 40}, 
+	{:name => :Blastoise, :attack => 80}, 
+	{:name => :Hitmonchan, :attack => 50}, 
+]
+
+card_pack.reverse!
+pile_one = card_pack.slice!(0,3)
+pile_two = card_pack.slice!(0,3)
+@pile_three = card_pack.slice!(0,3)
+@pile_four = card_pack.slice(0)
+
+@alfred_pocket = [].concat pile_one
+@peter_pocket = [].concat pile_two
+
+def arm_wrestle
+	chance_of_winning = rand
+	if chance_of_winning < 0.5
+		@alfred_pocket.concat @pile_three
+		@peter_pocket.push @pile_four
+	else
+		@peter_pocket.concat @pile_three
+		@alfred_pocket.push @pile_four
+	end
+end
+
+arm_wrestle
+
+puts "Peter has the following cards:"
+puts "#{@peter_pocket.each {|key| puts key}}"
+puts "Alfred has the following cards: "
+puts "#{@alfred_pocket.each {|key| puts key}}"
+
+# binding.pry
