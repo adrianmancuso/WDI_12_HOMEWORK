55745be79e5ad51493d4c2fd001c672dccf78455
Adrian <adrian.mancuso@gmail.com>
2017-07-09 18:30:30 +1000

    completed javascript refresher homework

--- Adrian/wk06/5-fri/select_box-master/starter_code/js/main.js
+++ Adrian/wk06/5-fri/select_box-master/starter_code/js/main.js
@@ -1,1 +1,22 @@
-var cities = [];
+var cities = ["NYC", "SF", "LA", "ATX", "SYD"];
+var body = document.getElementsByTagName('body')[0];
+var selectBox = document.getElementById('city-type');
+
+for (var i = cities.length - 1; i >= 0; i--) {
+	var option = document.createElement("OPTION");
+	option.textContent = cities[i];
+	selectBox.appendChild(option);
+};
+
+var changeBackground = function() {
+	var choice = event.target.value;
+	if (choice === "SYD") {
+		body.className = 'sydney';
+	} else if (choice === "ATX") {
+		body.className = 'austin';
+	} else {
+		body.className = choice.toLowerCase();
+	}
+};
+
+selectBox.addEventListener('change', changeBackground);
